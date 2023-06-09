f0cf4613bb4371a5933f1bd4404abebe6510d8c1
12alexa12a <133241498+12alexa12a@users.noreply.github.com>
2023-06-09 11:03:30 +0000

    gata vaaaa

---
+++ script.js
@@ -0,0 +1,15 @@
+function refreshKey() {
+    fetch('/key-rec')
+        .then(response => response.json())
+        .then(data => {
+            const newKey = data.key;
+            console.log(newKey);
+            document.getElementById('key').innerText = newKey;
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
+
+// canudbWrite("chei", "ups")
+// canudbRead("chei")
---
+++ server.js
@@ -0,0 +1,43 @@
+var fs = require('fs');
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+const canudb_db = "CanuDB/";
+
+function canudbWrite(table, data) {
+    console.log("[CanuDB] write > data " + data + " to table > " + table);
+    fs.writeFileSync(canudb_db + table + ".canu", data);
+}
+
+function canudbRead(table, callback) {
+    const recvData = fs.readFileSync(canudb_db + table + ".canu", "utf8");
+    console.log("[CanuDB] read > data " + recvData + " from table > " + table);
+    callback(recvData);
+}
+
+app.use(express.json());
+
+// Endpoint to fetch the key
+app.post('/key', (req, res) => {
+    const key = req.query.key;
+    console.log(key)
+    canudbWrite("chei", key);
+    res.send("Key received and written to the database.");
+});
+
+app.get('/key-rec', (req, res) => {
+    canudbRead("chei", function (key) {
+        console.log("Key received and read from the database.");
+        res.send({ key: key });
+    });
+});
+
+// Serve the index.html file
+app.use(express.static(__dirname));
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
